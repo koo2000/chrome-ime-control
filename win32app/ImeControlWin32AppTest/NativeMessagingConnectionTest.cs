3eab2e33d99f20231a3c0e2df66cda7f9fa42bb0
koichi asano <koo2000@gmail.com>
2026-04-12 17:45:29 +0900

    add  NativeMessagingConnection

---
+++ win32app/ImeControlWin32AppTest/NativeMessagingConnectionTest.cs
@@ -0,0 +1,60 @@
+﻿namespace Test
+
+{
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using NativeMessaging;
+    using NUnit.Framework.Interfaces;
+
+
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        public class TestListener : INativeMessagingEventListener
+        {
+            private readonly List<String> messages = new List<string>();
+
+            public List<string> Messages => messages;
+
+            public void messageReceived(string message)
+            {
+                messages.Add(message);
+                Console.Out.WriteLine("message receive [" + message + "]");
+            }
+        }
+        [Test]
+        public void TestInStream()
+        {
+
+            TestListener testListener = new TestListener();
+
+            byte[] data1 = Encoding.UTF8.GetBytes("message01 dayo");
+            byte[] data2 = Encoding.UTF8.GetBytes("message02 dayo dayo.");
+            byte[] dataLen1 = BitConverter.GetBytes((Int32)data1.Length);
+            byte[] dataLen2 = BitConverter.GetBytes((Int32)data2.Length);
+
+            MemoryStream inStream = new MemoryStream();
+            inStream.Write(dataLen1);
+            inStream.Write(data1);
+            inStream.Write(dataLen2);
+            inStream.Write(data2);
+            inStream.Seek(0, SeekOrigin.Begin);
+
+            NativeMessagingConnection con = new NativeMessagingConnection(inStream, null);
+            con.AddListener(testListener);
+
+            con.startRead();
+
+
+            Thread.Sleep(1000);
+
+
+            Assert.That("message01 dayo", Is.EqualTo(testListener.Messages[0]));
+            Assert.That("message02 dayo dayo.", Is.EqualTo(testListener.Messages[1]));
+        }
+    }
+}
