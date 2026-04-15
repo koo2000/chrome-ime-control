d685444398e4a9fe995560cd34811463d82cd5dc
koo2000 <koo2000@gmail.com>
2026-04-15 12:44:43 +0900

    他ユーザのプロセスに対して動作しないよう修正。 (#2)

--- nativeapp/ImeControlNativeApp/ImeController.cs
+++ nativeapp/ImeControlNativeApp/ImeController.cs
@@ -1,14 +1,16 @@
 
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Management;
 using System.Text;
 using NLog;
+using System.Runtime.Versioning;
+using System.ComponentModel;
 
 
 namespace ImeControl
 {
-
-
+    [SupportedOSPlatform("windows")]
     public class ImeController
     {
 
@@ -21,24 +23,44 @@ namespace ImeControl
 
         private const uint IME_CONTROL = 0x0283;
         private const int IME_SETOPENSTATUS = 0x0006;
-        
+
         public void setImeActiveStatus(String processName, Boolean imemode)
         {
+            String queryString = String.Format("SELECT * FROM Win32_Process Where Name like '{0}.exe'", processName);
+            // String queryString = String.Format("SELECT * FROM Win32_Process ");
+            var query = new ManagementObjectSearcher(
+                queryString
+                );
+            // var query = new ManagementObjectSearcher(@"SELECT * FROM Win32_Process ");
+            var result = query.Get();
             var processes = Process.GetProcessesByName(processName);
             IntPtr target_hwnd = IntPtr.Zero;
 
-            foreach (Process p in processes)
+            int cnt = 0;
+            foreach (ManagementObject o in result)
             {
-                IntPtr mainWindowHandle = p.MainWindowHandle;
-                if (mainWindowHandle != 0)
-                {
-                    log.Debug("process [" + processName + "] found. pid = " + p.Id + "]");
-                    IntPtr imeHandle = ImmGetDefaultIMEWnd(mainWindowHandle);
-                    SendMessage(imeHandle, IME_CONTROL, IME_SETOPENSTATUS, imemode ? 1 : 0);
-                    break;
+                Object[] user = new Object[2];
+                o.InvokeMethod("GetOwner", user);
+                String userName = (String)user[0];
 
+                // find only current user process
+                if (userName != null && userName.Length > 0)
+                {
+                    var id = o["ProcessId"];
+                    Process p = Process.GetProcessById(Convert.ToInt32(id));
+
+                    IntPtr mainWindowHandle = p.MainWindowHandle;
+                    if (mainWindowHandle != 0)
+                    {
+                        log.Debug("process [" + processName + "] found. pid = " + p.Id + "]");
+                        IntPtr imeHandle = ImmGetDefaultIMEWnd(mainWindowHandle);
+                        SendMessage(imeHandle, IME_CONTROL, IME_SETOPENSTATUS, imemode ? 1 : 0);
+                        break;
+
+                    }
                 }
             }
+
         }
 
     }
