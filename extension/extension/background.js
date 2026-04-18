aa40f5b8526f6c58750cab1c0b9c81a3e20f9306
koo2000 <koo2000@gmail.com>
2026-04-18 11:09:24 +0900

    ネイティブアプリとの接続断で再接続する機能を追加。 (#3)
    * 他ユーザのプロセスに対して動作しないよう修正。
    
    * プロセスが停止した際に再接続する機能を追加。

--- extension/extension/background.js
+++ extension/extension/background.js
@@ -1,26 +1,36 @@
 var agent = navigator.userAgent;
-console.log(agent);
-
+console.log("agent = " + agent);
 
 // ポートを開ける
-var port = chrome.runtime.connectNative("com.github.koo2000.imecontrol");
+class ImeControlConnection {
+  constructor() {
+    this.port = null;
+    // 画面からメッセージを受け取ってネイティブに渡す
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+      if (this.port == null) {
+        this.reconnect();
+      }
+      if ("imeOn" === message) {
+        this.port.postMessage({ "imemode": "on", "agent" : agent });
+      } else if ("imeOff" === message) {
+        this.port.postMessage({ "imemode": "off", "agent" : agent  });
+      }
+      sendResponse("request processed");
+    });
 
-port.onMessage.addListener((msg) => {
-  // TODO 戻りメッセージの処理（あれば）
-  console.log('Received' + msg);
-});
-port.onDisconnect.addListener(() => {
-  // TODO 接続時は再接続（？）
-  console.log('Disconnected');
-});
-
-
-// 画面からメッセージを受け取ってネイティブに渡す
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if ("imeOn" === message) {
-    port.postMessage({ "imemode": "on", "agent" : agent });
-  } else if ("imeOff" === message) {
-    port.postMessage({ "imemode": "off", "agent" : agent  });
   }
-  sendResponse("request processed");
-});
+  reconnect() {
+    this.port = chrome.runtime.connectNative("com.github.koo2000.imecontrol");
+    this.port.onMessage.addListener((msg) => {
+      console.log('ImeControlConnection: Received' + msg);
+    });
+    this.port.onDisconnect.addListener(() => {
+      console.log('ImeControlConnection: Disconnected');
+      this.port = null;
+    });
+
+  }
+}
+
+
+new ImeControlConnection();
